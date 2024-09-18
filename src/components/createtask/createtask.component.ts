6786e2ff80ad781388bdfb827a737c66e0e9d28d
ImranGreet <88135011+ImranGreet@users.noreply.github.com>
2024-09-18 20:51:41 +0600

    task and interface is defined

--- src/components/createtask/createtask.component.ts
+++ src/components/createtask/createtask.component.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
+import Task from '../../app/Model/Medicine';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-createtask',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './createtask.component.html',
-  styleUrl: './createtask.component.scss'
+  styleUrl: './createtask.component.scss',
+  template: `
+    <profile-photo />
+    <button>Upload a new profile photo</button>`,
 })
+
 export class CreatetaskComponent {
 
   userName:string = "";
   userSex:string ="";
   userAge:Number = 0;
   userPhoneNumber:string ="";
+
+ prescribedMedicine:Task[] =[];
+ addTask(){
+  const newTask: Task = {
+    id: this.prescribedMedicine.length + 1, // Incremental id
+    title: '',
+    status: 'pending',
+    initialStartDate: new Date(),
+    dueDate: new Date(),
+    doseCompletedDate: new Date(),
+    mealStatus: 'Not Specific',
+    description: '',
+    formOfDose: ''
+  };
+  this.prescribedMedicine.push(newTask);
+  console.log(this.prescribedMedicine);
+ }
 }
